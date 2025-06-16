d24467f7d5b6134a09199071abe62168e3ebde49
K4izuo <crisjutineoracion22@gmail.com>
2025-06-16 16:12:25 +0800

    frontend: added a landing page and some components

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import { Routes, Route } from "react-router-dom";
+import MainPage from "./pages/MainPage";
 
 function App() {
   return (
     <Routes>
-      {/* <Route path="/" element={<Home />} />
-      <Route path="/about" element={<About />} /> */}
+      <Route path="/" element={<MainPage />} />
+      {/* <Route path="/about" element={<About />} /> */}
     </Routes>
   );
 }
