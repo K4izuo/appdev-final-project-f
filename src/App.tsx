d718faf973c0848ef07a685b0d0298374574d963
K4izuo <crisjutineoracion22@gmail.com>
2025-06-16 11:40:26 +0800

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { Routes, Route } from "react-router-dom";
+
+function App() {
+  return (
+    <Routes>
+      {/* <Route path="/" element={<Home />} />
+      <Route path="/about" element={<About />} /> */}
+    </Routes>
+  );
+}
+
+export default App;
